5fdce61789d8e44d290ac3cf6a4a2e09cbf62e57
Said Al Souti <saidalsouti@gmail.com>
2024-02-02 23:53:10 +0400

    Add restart option for certbot and bind-address option for mysql

--- publish.sh
+++ publish.sh
@@ -5,10 +5,13 @@ docker context create remote_ticket --docker host=ssh://root@45.76.36.193
 docker context use remote_ticket 
 
 docker ps   # list running containers
-scp -r conf root@45.76.36.193:/root   # copy new docker-compose
+scp -r server/nginx root@45.76.36.193:/root   # copy new docker-compose
+scp -r server/mysql-init root@45.76.36.193:/root/mysql-init
 
 # docker compose -f Deployment/docker-compose.yml pull
+# docker compose -f Deployment/docker-compose.yml up -d --no-deps --build mysql
 docker compose -f Deployment/docker-compose.yml up -d   # running docker
+
 
 docker ps
 
